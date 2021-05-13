3f4f035bba3acdba7e62bb702102093f5135d0a3
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2021-05-13 19:41:21 -0400

    WIP: Sign In page

--- sick-fits/frontend/components/Products.js
+++ sick-fits/frontend/components/Products.js
@@ -7,16 +7,16 @@ import Product from "./Product";
 export const ALL_PRODUCTS_QUERY = gql`
     query ALL_PRODUCTS_QUERY($skip: Int = 0, $first : Int) {
         allProducts(first: $first, skip: $skip) {
-        id
-        name
-        price
-        description
-        photo {
             id
-            image {
-            publicUrlTransformed
+            name
+            price
+            description
+            photo {
+                id
+                image {
+                    publicUrlTransformed
+                }
             }
-        }
         }
     }
 `;
