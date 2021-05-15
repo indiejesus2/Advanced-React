f252183ea979b2068b32d826227f743e41a5c59a
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2021-05-15 08:56:49 -0400

    WIP: SignOut Capability added"

--- sick-fits/frontend/components/SignIn.js
+++ sick-fits/frontend/components/SignIn.js
@@ -3,6 +3,7 @@ import { useMutation } from '@apollo/client';
 import Form from './styles/Form';
 import useForm from '../lib/useForm';
 import { CURRENT_USER_QUERY } from './User';
+import Error from './ErrorMessage';
 
 const SIGNIN_MUTATION = gql`
   mutation SIGNIN_MUTATION($email: String!, $password: String!) {
@@ -13,6 +14,10 @@ const SIGNIN_MUTATION = gql`
           email
           name
         }
+      }
+      ... on UserAuthenticationWithPasswordFailure {
+        code
+        message
       }
     }
   }
@@ -25,15 +30,25 @@ export default function SignIn() {
   });
   const [signin, { data, loading }] = useMutation(SIGNIN_MUTATION, {
     variables: inputs,
+    // refetch the currently logged in user
     refetchQueries: [{ query: CURRENT_USER_QUERY }],
   });
   async function handleSubmit(e) {
     e.preventDefault(); // stop the form from submitting
+    console.log(inputs);
     const res = await signin();
+    console.log(res);
+    resetForm();
+    // Send the email and password to the graphqlAPI
   }
+  const error =
+    data?.authenticateUserWithPassword.__typename === 'UserAuthenticationWithPasswordFailure'
+      ? data?.authenticateUserWithPassword
+      : undefined;
   return (
     <Form method="POST" onSubmit={handleSubmit}>
       <h2>Sign Into Your Account</h2>
+      <Error error={error} />
       <fieldset>
         <label htmlFor="email">
           Email
